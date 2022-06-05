b598dea60d4c745c42333c93020feb317c9919e4
Wandia E Mugo <wwandia173@gmail.com>
2022-06-05 19:10:07 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 // Write your solution here!
+let cats=["Milo" ,"Otis", "Garfield"];
+
+function destructivelyAppendCat() {
+    cats.push("Ralph");
+
+}
+
+function destructivelyPrependCat() {
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(2);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(0);
+}
+
+function appendCat(name) {
+    let newCat;
+    newCat = ([...cats, "Broom"]);
+    return newCat;
+}
+appendCat("Broom");
+
+function prependCat(name) {
+    let newCat;
+    newCat = (["Arnold",...cats]);
+    return newCat;
+}
+prependCat("Arnold");
+
+function removeLastCat(){
+    let newArray;
+    newArray = (["Milo", "Otis"]);
+    newArray.splice(2,"Garfield");
+    return newArray;
+}
+removeLastCat();
+
+function removeFirstCat(){
+    let newArray;
+    newArray = (["Otis", "Garfield"]);
+    newArray.splice(0,"Milo");
+    return newArray;
+}
+removeFirstCat();
